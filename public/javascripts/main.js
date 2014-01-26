2116c9cbec9e4b2d29e28b7979ec4ec10fa81288
Matt Kingston <mattkingston+github@gmail.com>
2014-01-26 15:52:11 +0000

    Splash updated

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -151,7 +151,7 @@ function create () {
                      "survival is relying on the light of battle to survive. While " +
                      "your allies will do their best to help you, you must fend for " +
                      "yourself to wipe out the enemy ships. While firing your weapons " +
-                     "illuminates a path, it also draw the attention of the enemies.", 30, 0,
+                     "illuminates a path, it also draw the attention of the enemies.", 15, 0,
                      function() { splashText(game, "Use the arrow keys to move, space-bar to shoot.", 5, 0); }
                      );
 }
